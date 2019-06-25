b2b24ae4af3ca7b04470c0614510981ff0b7fd7a
Alan Zhong <alan.zhong.51@gmail.com>
2019-06-25 13:40:06 -0400

    Added implementations for all Stack member functions. Still have debugging to do

---
+++ build/src/CMakeFiles/hw4a_library.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw4a_library.dir/hw4q1.cpp.o"
+  "CMakeFiles/hw4a_library.dir/hw4q4.cpp.o"
+  "libhw4a_library.pdb"
+  "libhw4a_library.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hw4a_library.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/hw4q1.cpp
+++ src/hw4q1.cpp
@@ -1,4 +1,73 @@
+#ifndef __HW4Q1_CPP__
+#define __HW4Q1_CPP__
+
 #include "hw4q1.h"
 
+template <class Item>
+Stack<Item>::Stack() {
+    capacity = DEF_CAP;
+    used = 0;
+    data = new Item[capacity];
+}
 
+template <class Item>
+Stack<Item>::Stack(const Stack<Item>& source) {
+    capacity = source.capacity;
+    used = source.used;
+    data = new Item[capacity];
+    for (int i = 0; i < used; i++) {
+        data[i] = source.data[i];
+    }
+}
 
+template <class Item>
+Stack<Item>::~Stack() {
+    delete[] data;
+}
+
+template <class Item>
+void Stack<Item>::push(const Item& entry) {
+    if (used == capacity) {
+        return;
+    }
+    data[used] = entry;
+    used++;
+}
+
+template <class Item>
+void Stack<Item>::pop() {
+    if (used < 1) {
+        return;
+    }
+    //delete data[used - 1];
+    used--;
+}
+
+template <class Item>
+void Stack<Item>::operator=(const Stack<Item>& source) {
+    this->used = source.used;
+    this->capacity = source.capacity;
+    this->data = new Item[this->capacity];
+    for (int i = 0; i < this->used; i++) {
+        this->data[i] = source.data[i];
+    }
+}
+
+template <class Item>
+size_t Stack<Item>::size() const {
+    return used;
+}
+
+template <class Item>
+bool Stack<Item>::empty() const {
+    return used == 0;
+}
+
+template <class Item>
+Item Stack<Item>::top() const {
+    if (empty()) {
+        return Item();
+    }
+    return data[used-1];
+}
+#endif
