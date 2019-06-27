5f68e8be5d0778473e3fa8dba48731bf5a782c9c
Alan Zhong <alan.zhong.51@gmail.com>
2019-06-26 23:25:35 -0400

    Added constructor and push() for Stack2Queue

--- src/hw4q4.cpp
+++ src/hw4q4.cpp
@@ -3,5 +3,27 @@
 
 #include "hw4q4.h"
 
+template <class Item>
+Stack2Queue<Item>::Stack2Queue() {
+    s1 = Stack();
+    s2 = Stack();
+    used = 0;
+}
+
+template <class Item>
+void Stack2Queue<Item>::push(const Item& entry) {
+    while (!s1.empty()) {
+        s2.push(s1.top());
+        s1.pop();
+    }
+    s1.push(entry);
+    while (!s2.empty()) {
+        s1.push(s2.top());
+        s2.pop();
+    }
+}
+
+
+
 #endif
 
